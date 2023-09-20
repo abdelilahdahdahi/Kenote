1dce13a1228ef85b72043718876bb4249709d1ea
abdelilah <a.dahdahi@ieengsolution.it>
2023-09-20 23:47:27 +0200

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,21 @@ import React from 'react';
 import Header from "./Header";
 import Note from "./Note";
 import Footer  from './Footer';
+import notes from '../notes';
 
+function createNote(notes) {
+    return <Note 
+    key = {notes.id}
+    id = {notes.id}
+    title = {notes.title}
+    content = {notes.content}
+    />
+}
 function App() {
     return (
         <div>
             <Header /> 
-            <Note />
+            {notes.map(createNote)}
             <Footer />
         </div>
     );
---
+++ src/notes.js
@@ -0,0 +1,24 @@
+const notes = [
+    {
+        id: 1,
+        title: "Note 1",
+        content: "This is note 1"
+    },
+    {
+        id: 2,
+        title: "Note 2",
+        content: "This is note 2"
+    },
+    {
+        id: 3,
+        title: "Note 3",
+        content: "This is note 3"
+    },
+    {
+        id: 4,
+        title: "Note 4",
+        content: "This is note 4"
+    },
+];
+
+export default notes;
