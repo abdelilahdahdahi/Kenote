2901527cebcfce829a2c3d40ad814f9a31c1e4e0
abdelilah <a.dahdahi@ieengsolution.it>
2023-09-22 17:02:27 +0200

    change 1

--- src/notes.js
+++ src/notes.js
@@ -1,24 +1,34 @@
 const notes = [
     {
-        id: 1,
-        title: "Note 1",
-        content: "This is note 1"
+        key: 1,
+        title: "Delegation",
+        content:
+        "Q. How many programmers does it take to change a light bulb? A. None – It’s a hardware problem"
     },
     {
-        id: 2,
-        title: "Note 2",
-        content: "This is note 2"
+        key: 2,
+        title: "Loops",
+        content:
+        "How to keep a programmer in the shower forever. Show him the shampoo bottle instructions: Lather. Rinse. Repeat."
     },
     {
-        id: 3,
-        title: "Note 3",
-        content: "This is note 3"
+        key: 3,
+        title: "Arrays",
+        content:
+          "Q. Why did the programmer quit his job? A. Because he didn't get arrays."
     },
     {
-        id: 4,
-        title: "Note 4",
-        content: "This is note 4"
+        key: 4,
+        title: "Hardware vs. Software",
+        content:
+          "What's the difference between hardware and software? You can hit your hardware with a hammer, but you can only curse at your software."
     },
+    {
+        key: 5,
+        title: "Kubernetes VS Docker",
+        content:
+          "What's the difference between them? "
+    }
 ];
 
 export default notes;
