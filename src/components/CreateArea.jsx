092b0119957eb43b0b2ba7dc54c4a19631385f82
abdelilah <a.dahdahi@ieengsolution.it>
2023-10-01 10:52:14 +0200

    app functionality done

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CreateArea() {
+function CreateArea(props) {
+
+  const [note, setNote] = useState({
+    title : "",
+    content : ""
+  });
+  
+  function handleNoteChange(e){
+    const {name, value} = e.target;  //destructuring object
+    
+    setNote(prevNote => {
+      return {...prevNote, [name]: value};
+    });
+  }
+
+  function submitNote(event) {
+    props.onAdd(note)
+    setNote({
+      title: "",
+      content: ""
+    })
+    event.preventDefault();
+  }
+
   return (
     <div>
-      <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+      <form >
+        <input onChange={handleNoteChange} name="title" placeholder="Title" value={note.title} />
+        <textarea onChange={handleNoteChange} name="content" placeholder="Take a note..." rows="3" value={note.content} />
+        <button onClick={submitNote}>Add</button>
       </form>
     </div>
   );
