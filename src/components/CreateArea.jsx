79d1bc3d9cb8c75cc1f667c0137a86fe76401957
abdelilah <a.dahdahi@ieengsolution.it>
2023-10-01 10:56:11 +0200

    setting project from sandbox

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,34 +1,47 @@
 import React, { useState } from "react";
 
 function CreateArea(props) {
-
   const [note, setNote] = useState({
-    title : "",
-    content : ""
+    title: "",
+    content: ""
   });
-  
-  function handleNoteChange(e){
-    const {name, value} = e.target;  //destructuring object
-    
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
     setNote(prevNote => {
-      return {...prevNote, [name]: value};
+      return {
+        ...prevNote,
+        [name]: value
+      };
     });
   }
 
   function submitNote(event) {
-    props.onAdd(note)
+    props.onAdd(note);
     setNote({
       title: "",
       content: ""
-    })
+    });
     event.preventDefault();
   }
 
   return (
     <div>
-      <form >
-        <input onChange={handleNoteChange} name="title" placeholder="Title" value={note.title} />
-        <textarea onChange={handleNoteChange} name="content" placeholder="Take a note..." rows="3" value={note.content} />
+      <form className="create-note">
+        <input
+          name="title"
+          onChange={handleChange}
+          value={note.title}
+          placeholder="Title"
+        />
+        <textarea
+          name="content"
+          onChange={handleChange}
+          value={note.content}
+          placeholder="Take a note..."
+          rows="3"
+        />
         <button onClick={submitNote}>Add</button>
       </form>
     </div>
