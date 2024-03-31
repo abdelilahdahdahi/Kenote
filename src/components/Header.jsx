0b46469b36441b5647d7f1477e310e2f57fe48ce
abdelilah <dahdahiabdelilah2002@gmail.com>
2024-03-31 18:42:37 +0200

    rebranding

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import EditNoteIcon from '@mui/icons-material/EditNote';
 
 function Header () {
     return (
         <header>
-            <h1>Keeper App <EditNoteIcon /></h1>
+            <img src="./images/header.png" alt="header" />
         </header>
     );
 }
